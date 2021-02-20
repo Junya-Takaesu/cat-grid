462db0d926402b389c7d39e71474c1ce2a5b5ac5
gandhi <ryokuchamugichauroncha.tea5@gmail.com>
2021-02-21 00:53:21 +0900

    Finish

--- index.rb
+++ index.rb
@@ -5,7 +5,7 @@ require_relative "./app/ruby/cat_images_client.rb"
 get "/" do
   cat_client = CatImagesClient.new
 
-  query_params = {size: "small",limit: 10}
+  query_params = {size: "small",limit: 8}
 
   cat_client.request(endpoint: "/images/search", query_params: query_params)
   @image_urls = cat_client.images
